7e19146ed76d79460e3f35667d173e3942dab2f5
abtechbit <abtechbit@users.noreply.github.com>
2020-12-24 18:18:02 -0500

    add header component

---
+++ theme/js/comp/app-bar-header.js
@@ -0,0 +1,14 @@
+Vue.component('app-bar-header', {
+    template: '#app-bar-header-template',
+  
+    data: () => ({
+    }),
+  
+    beforeMount: function(){  
+    },
+    computed: {
+    },
+    methods: {
+    }
+  });
+  
